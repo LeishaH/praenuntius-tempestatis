7efcd65a492c29676ddbbd8748e9c20e70292f57
LeishaCodeCraft <leisha1234567@gmail.com>
2025-05-17 19:54:16 +1000

    Updated API. Corrected the way temperature data is accessed from API response. Previously used a wrong property, causing the temperature not to update.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let detailsElement = document.querySelector(".weather-app-details");
+
+  let temperature = Math.round(response.data.temperature.current);
+  let humidity = response.data.temperature.humidity;
+  let wind = Math.round(response.data.wind.speed);
+  let description = response.data.condition.description;
+  let date = new Date(response.data.time * 1000);
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
 
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = temperature;
+  detailsElement.innerHTML = `
+    ${date.toLocaleDateString("en-US", {
+      weekday: "long",
+    })} ${hours}:${minutes}, ${description}
+    <br />
+    Humidity: <strong>${humidity}%</strong>, Wind: <strong>${wind} km/h</strong>
+  `;
 }
 
 function searchCity(city) {
-  let apiKey = "YOUR_API_KEY_HERE";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  const apiKey = "tf950d82b50b8f88a15d0ac93o492d93";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-
+  const searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+const searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney");
