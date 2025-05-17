ebfca4362b327c0a9e42559b0e86a172092f17b2
LeishaCodeCraft <leisha1234567@gmail.com>
2025-05-17 16:55:38 +1000

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+function searchCity(city) {
+  let apiKey = "tf950d82b50b8f88a15d0ac93o492d93";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-
