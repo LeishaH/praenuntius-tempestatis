f1cb3b653b280a678da93f4847f1333cb81aaec6
LeishaCodeCraft <leisha1234567@gmail.com>
2025-05-24 12:43:51 +1000

    Updated JavaScript (added slice in code) so that forecast would start day after current date.

--- src/index.js
+++ src/index.js
@@ -69,25 +69,23 @@ function formatDay(timestamp) {
 function displayForecast(response) {
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
-      forecastHtml += `
-  <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${formatDay(day.time)}</div>
-    <div class="weather-forecast-icon">
-      <img src="${day.condition.icon_url}" alt="forecast icon" />
-    </div>
-    <div class="weather-forecast-temperatures">
-      <div class="weather-forecast-temperature">
-        <strong>${Math.round(day.temperature.maximum)}°</strong>
+  response.data.daily.slice(1, 6).forEach(function (day) {
+    forecastHtml += `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+          <img src="${day.condition.icon_url}" alt="forecast icon" />
+        </div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
+          </div>
+          <div class="weather-forecast-temperature">
+            ${Math.round(day.temperature.minimum)}°
+          </div>
+        </div>
       </div>
-      <div class="weather-forecast-temperature">${Math.round(
-        day.temperature.minimum
-      )}º</div>
-    </div>
-  </div>
-`;
-    }
+    `;
   });
 
   let forecastElement = document.querySelector("#forecast");
